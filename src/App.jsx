26eedb32943ba6839a73e9f400937737ecaaaf11
gabriel degobi <gwillians98@gmail.com>
2025-07-04 17:21:00 -0300

    ADD REQUISICAO POST

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import style from './App.module.css'
 
 
 
@@ -7,8 +7,10 @@ const url = 'http://localhost:3000/products'
 
 function App() {
   const [products, setProducts] = useState([])
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
 
-
+  //1- RESGATANDO DADOS 
   useEffect(() => {
     async function fecthData() {
       const res = await fetch(url)
@@ -18,8 +20,26 @@ function App() {
     fecthData()
   }, [])
 
+  //ADD DE PRODUTOS
+  const handleSubmit = async (e) => {
+    e.preventDefault()
 
-  console.log(products)
+    const product = {
+      //como os states tem o mesmo nome dos objetos
+      //eu nao preciso colocar a chave e o valor, podemos resumir
+      name,
+      price
+    }
+    //REQUISIÇÃO POST
+    const res = await fetch(url,{
+      method:"POST", //AQUI COLOCAMOS O METODO DA REQUISIÇÃO, DIFERENTE DO GET QUE JA VEM PADRÃO
+      headers:{//CABECALHO DA REQUISIÇÃO
+        "Content-Type":"aplication/json"
+      },
+      body:JSON.stringify(product),
+    })
+
+   }
 
 
   return (
@@ -27,13 +47,34 @@ function App() {
       <h1>Lista de Produtos</h1>
       <ul>
         {
-          products.map((product)=>(
+          products.map((product) => (
             <li key={product.id}>
               {product.name} - R$: {product.price}
             </li>
           ))
         }
       </ul>
+      <div className={style.add_product}>
+        <form onSubmit={handleSubmit}>
+          <label >
+            Nome:
+            <input 
+            type="text" 
+            value={name} 
+            name='name' 
+            onChange={(e)=>setName(e.target.value)}/>
+          </label>
+          <label >
+            Preço:
+            <input 
+            type="number" 
+            value={price} 
+            name='price' 
+            onChange={(e)=>setPrice(e.target.value)}/>
+          </label>
+          <input type="submit" value='criar' />
+        </form>
+      </div>
     </div>
   )
 }
